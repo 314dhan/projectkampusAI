5db890c30ba740459c3b32d388d1ff2196f9b5de
314dhan <ramadhan25321@gmail.com>
2023-11-11 16:56:22 +0700

    a

--- index.php
+++ index.php
@@ -1,44 +1,41 @@
 <!DOCTYPE html>
 <html lang="id">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
+    <title>Beranda</title>
     <!-- Bootstrap CDN -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
-
 <body>
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-6">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-3">
+                <div class="list-group mt-5">
+                    <a href="#" class="list-group-item list-group-item-action active">BERANDA</a>
+                    <a href="#" class="list-group-item list-group-item-action">DATA PENGGUNA</a>
+                    <a href="datakerusakan.php" class="list-group-item list-group-item-action">DATA KERUSAKAN</a>
+                    <a href="datagejala.php" class="list-group-item list-group-item-action">DATA GEJALA</a>
+                    <a href="#" class="list-group-item list-group-item-action">BASIS PENGETAHUAN</a>
+                    <hr style="width:100%;text-align:left;margin-left:0">
+                    <a href="#" class="list-group-item list-group-item-action">HASIL KONSULSTASI</a>
+                </div>
+            </div>
+            <div class="col-md-9">
                 <div class="card mt-5">
                     <div class="card-header">
-                        <h3 class="text-center">Login</h3>
+                        <h3 class="text-center">Diagnosa Kerusakan Kendaraan pada Motor Matic Honda</h3>
                     </div>
                     <div class="card-body">
-                        <form action="" method="post">
-                            <div class="form-group">
-                                <label for="email">Email</label>
-                                <input type="email" name="email" id="email" class="form-control" required>
-                            </div>
-                            <div class="form-group">
-                                <label for="password">Password</label>
-                                <input type="password" name="password" id="password" class="form-control" required>
-                            </div>
-                            <div class="form-group">
-                                <button type="submit" name="login" class="btn btn-primary btn-block">Login</button>
-                            </div>
-                        </form>
+                        <p class="text-center">Ini adalah halaman beranda yang menampilkan list BERANDA, DATA PENGGUNA, BASIS PENGETAHUAN di samping halaman page yang vertikal.</p>
                     </div>
-                    <div class="card-footer">
-                        <p class="text-center">Belum punya akun? <a href="register.html">Register</a></p>
-                    </div>
+                </div>
+                <div>
+                    <p>NOTE: data pengguna</p>
+                    <p>nama, email, jenis motor, no hp, alamat, varian motor, tahun pembuatan motor, jenis service</p>
                 </div>
             </div>
         </div>
     </div>
 </body>
-
 </html>
